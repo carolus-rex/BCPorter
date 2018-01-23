4923d934dd747d3e594a063ccaa0bcd7596a1ca2
Gogs <gogs@fake.local>
2018-01-22 19:12:11 -0500

    Added blank report processing

--- bcporter.py
+++ bcporter.py
@@ -3,6 +3,7 @@ import os
 import re
 
 from PIL import Image, ImageFont, ImageDraw
+from docxtpl import DocxTemplate
 
 CHAR_BACKSPACE = re.compile(".\b")  # Don't use a raw string here
 ANY_BACKSPACES = re.compile("\b+")  # or here
@@ -12,6 +13,10 @@ COMMAND_OUTPUT_SEPARATOR = '-' * 100 + '\n'
 FOREGROUND = (255, 255, 255)
 WIDTH = 375
 HEIGHT = 50
+
+LOG = 0
+ATP = 1
+BLANK = 2
 
 
 def create_dir_if_not_exists(directory):
@@ -46,10 +51,13 @@ def process_non_printable(s):
 
 def get_commands():
     with open("show_commands.txt") as show_commands_file:
-        commands = [(command.strip(), False) for command in show_commands_file.readlines()]
+        commands = [(command.strip(), LOG) for command in show_commands_file.readlines()]
 
     with open("atp_commands.txt") as atp_commands_file:
-        commands.extend((command.strip(), True) for command in atp_commands_file.readlines())
+        commands.extend((command.strip(), ATP) for command in atp_commands_file.readlines())
+
+    with open("blank_commands.txt") as blank_commands_file:
+        commands.extend((command.strip(), BLANK) for command in blank_commands_file.readlines())
 
     return commands
 
@@ -91,7 +99,15 @@ def lines_to_png(lines, file_name):
         draw.text((0, index * height), line, font=font, fill=FOREGROUND)
 
     x.show()
-    x.save('%s.png' % file_name)
+
+    image_path = '%s.png' % file_name
+    x.save(image_path)
+
+    return image_path
+
+
+def place_image_into_word(tpl, pic_placeholder, image_path):
+    tpl.replace_pic(pic_placeholder, image_path)
 
 
 def process_log(log_to_process, prompt):
@@ -102,7 +118,7 @@ def process_log(log_to_process, prompt):
     reset_file(final_report)
 
     should_do_atp = log_to_process.endswith('Despues')
-
+    should_blank = 'BLANQUEAMIENTO' in log_to_process
     # with open(log_to_process) as log:
     #     for line in log.readlines():
     #         new_line = process_non_printable(line)
@@ -113,8 +129,25 @@ def process_log(log_to_process, prompt):
 
     atp_command_index = 1
 
-    for command, is_atp in commands:
-        if is_atp and not should_do_atp:
+    if should_blank:
+        state = "despues" if log_to_process.endswith("Despues") else "antes"
+
+        blank_context = {}
+
+        if state == "antes":
+            blank_report = DocxTemplate("BLANK_TEMPLATE_Antes.docx")
+            blank_context["atm_name"] = prompt[:-1]
+        else:
+            blank_report = DocxTemplate("BLANK_TEMPLATE_Despues.docx")
+
+    for command, command_type in commands:
+        if command_type == ATP and not should_do_atp:
+            continue
+
+        if command_type != BLANK and should_blank:
+            continue
+
+        if command_type == BLANK and not should_blank:
             continue
 
         command_outputs = get_command_output(command, prompt, log_to_process)
@@ -145,18 +178,35 @@ def process_log(log_to_process, prompt):
                     continue
             print("****************************FINISHED COMMAND %s*************************" % command)
 
-        if is_atp:
+        if command_type == ATP:
             data = list(command_outputs[index])
             data.append(prompt)
 
             lines_to_png(data, 'output_dir/ATP%i' % atp_command_index)
             atp_command_index += 1
+
+        elif command_type == BLANK:
+            underscore_command = command.replace(" ", "_")
+            blank_context[underscore_command + "_%s" % (state)] = ''.join(command_outputs[index])
+
+            if state == "antes":
+                blank_context[underscore_command + "_despues"] = "{{%s_despues}}" % underscore_command
+
         else:
             with open(final_report, 'a') as final_report_file:
                 for line in command_outputs[index]:
                     final_report_file.write(line)
 
                 final_report_file.write(COMMAND_OUTPUT_SEPARATOR)
+
+    if should_blank:
+        blank_report.render(blank_context)
+
+        if state == "antes":
+            blank_report.save("BLANK_TEMPLATE_Despues.docx")
+        else:
+            blank_report.save("output_dir/blank_report.docx")
+
 
     print("*******************************FINISHED*****************************")
 
